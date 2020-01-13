5daae0fe3aa5b55b0d4c3156fc4cc715079bc0da
Jason Lee <38192767+ShoesForClues@users.noreply.github.com>
2020-01-12 18:45:21 -0700

    Add files via upload

--- eztask.lua
+++ eztask.lua
@@ -37,13 +37,12 @@ local eztask={
 	_version = {2,1,5};
 	imports  = {};
 	threads  = {};
-	step_frequency = 1/60;
 }
 
 eztask.imports.eztask=eztask --wtf
 eztask._scope=setmetatable({},{
 	__index=function(_,k)
-		if k~="_instance" then
+		if k~="_thread" then
 			return eztask.current_thread[k] or eztask.current_thread.imports[k]
 		end
 		return eztask.current_thread
@@ -58,6 +57,77 @@ eztask.tick    = os.clock
 local thread   = {}
 local signal   = {}
 local property = {}
+
+------------------------------[Signal Class]------------------------------
+signal.__index=signal
+
+function signal.new()
+	return setmetatable({callbacks={}},signal)
+end
+
+function signal.attach(_signal,call,no_thread)
+	assert(type(call)=="function",("Cannot attach %s to callback."):format(type(call)))
+	local callback={index=#_signal.callbacks+1,call=call}
+	function callback:detach()
+		for i=self.index,1,-1 do
+			if _signal.callbacks[i]==self then
+				remove(_signal.callbacks,i);break
+			end
+		end
+		if self.parent_thread~=nil and self.parent_thread~=eztask then
+			self.parent_thread.callbacks[self]=nil
+		end
+	end
+	_signal.callbacks[callback.index]=callback
+	if not no_thread then
+		if eztask.current_thread then
+			callback.parent_thread=eztask.current_thread
+			eztask.current_thread.callbacks[callback]=callback
+		else
+			callback.parent_thread=eztask
+		end
+	end
+	return callback
+end
+
+function signal.invoke(_signal,...)
+	for _,callback in pairs(_signal.callbacks) do
+		if callback.parent_thread then
+			local args={...}
+			thread.new(function()
+				callback.call(unpack(args))
+			end,callback.parent_thread)()
+		else
+			callback.call(...)
+		end
+	end
+end
+
+function signal.detach(_signal)
+	for _,callback in pairs(_signal.callbacks) do
+		callback:detach()
+	end
+end
+
+------------------------------[Property Class]------------------------------
+property.__index=function(_property,k)
+	if k=="value" then
+		return rawget(_property,"_value")
+	end
+	return signal[k]
+end
+property.__newindex=function(_property,k,v)
+	assert(k=="value",("Cannot assign %s to property"):format(k))
+	local old=rawget(_property,"_value")
+	if v~=old then
+		rawset(_property,"_value",v)
+		_property:invoke(v,old)
+	end
+end
+
+function property.new(value)
+	return setmetatable({callbacks={},_value=value},property)
+end
 
 ------------------------------[Thread Class]------------------------------
 thread.__index=thread
@@ -86,7 +156,6 @@ function thread.new(env,parent_thread)
 		killed        = signal.new();
 		resume_state  = false;
 		tick          = 0;
-		real_tick     = 0;
 		resume_tick   = 0;
 		usage         = 0;
 		parent_thread = parent_thread or eztask.current_thread or eztask;
@@ -164,24 +233,21 @@ function thread.resume(_thread,dt,...)
 		end
 	end
 	if _thread.running.value==true then
-		_thread.tick=_thread.tick+(dt or 0)
-		_thread.real_tick=eztask.tick()
-		
+		dt=dt or 0
 		local previous_thread=eztask.current_thread
 		eztask.current_thread=_thread
-		
+		local real_tick=eztask.tick()
+		_thread.tick=_thread.tick+dt
 		if status(_thread.coroutine)=="suspended" and _thread.resume_tick<=_thread.tick then
 			local success,err=resume(_thread.coroutine,eztask._scope,...)
 			if not success then
 				print("[ERROR]: "..traceback(_thread.coroutine,err,1))
 			end
 		end
-		
 		for _,sub_thread in pairs(_thread.threads) do
 			sub_thread:resume(dt)
 		end
-		
-		_thread.usage=(eztask.tick()-_thread.real_tick)/eztask.step_frequency
+		_thread.usage=(eztask.tick()-real_tick)/dt
 		eztask.current_thread=previous_thread
 	end
 end
@@ -197,121 +263,6 @@ function thread.delete(_thread)
 	_thread.imports={}
 	_thread.parent_thread.threads[_thread.coroutine]=nil
 	_thread.killed:invoke()
-end
-
-------------------------------[Signal Class]------------------------------
-signal.__index=signal
-
-function signal.new()
-	return setmetatable({callbacks={}},signal)
-end
-
-function signal.attach(_signal,call,no_thread)
-	assert(type(call)=="function",("Cannot attach %s to callback."):format(type(call)))
-	local callback={index=#_signal.callbacks+1,call=call}
-	function callback:detach()
-		for i=self.index,1,-1 do
-			if _signal.callbacks[i]==self then
-				remove(_signal.callbacks,i);break
-			end
-		end
-		if self.parent_thread~=nil and self.parent_thread~=eztask then
-			self.parent_thread.callbacks[self]=nil
-		end
-	end
-	_signal.callbacks[callback.index]=callback
-	if not no_thread then
-		if eztask.current_thread then
-			callback.parent_thread=eztask.current_thread
-			eztask.current_thread.callbacks[callback]=callback
-		else
-			callback.parent_thread=eztask
-		end
-	end
-	return callback
-end
-
-function signal.invoke(_signal,...)
-	for _,callback in pairs(_signal.callbacks) do
-		if callback.parent_thread then
-			local args={...}
-			thread.new(function()
-				callback.call(unpack(args))
-			end,callback.parent_thread)()
-		else
-			callback.call(...)
-		end
-	end
-end
-
-function signal.detach(_signal)
-	for _,callback in pairs(_signal.callbacks) do
-		callback:detach()
-	end
-end
-
-------------------------------[Property Class]------------------------------
-property.__index=function(_property,k)
-	if k=="value" then
-		return rawget(_property,"_value")
-	end
-	return signal[k]
-end
-property.__newindex=function(_property,k,v)
-	assert(k=="value",("Cannot assign %s to property"):format(k))
-	local old=rawget(_property,"_value")
-	if v~=old then
-		rawset(_property,"_value",v)
-		_property:invoke(v,old)
-	end
-end
-
-function property.new(value)
-	return setmetatable({callbacks={},_value=value},property)
-end
-
---Legacy
-function eztask.new_signal()
-	return setmetatable({callbacks={}},signal)
-end
-
-function eztask.new_property(value)
-	return setmetatable({callbacks={},_value=value},property)
-end
-
-function eztask.new_thread(env,parent_thread)
-	if type(env)~="function" then env=eztask.require(env) or env end
-	assert(type(env)=="function","Cannot create thread with invalid environment")
-	
-	local _thread={
-		running       = eztask.new_property(false);
-		killed        = eztask.new_signal();
-		resume_state  = false;
-		tick          = 0;
-		real_tick     = 0;
-		resume_tick   = 0;
-		usage         = 0;
-		parent_thread = parent_thread or eztask;
-		env           = env;
-		threads       = {};
-		callbacks     = {};
-		imports       = {};
-	}
-	
-	_thread.running:attach(function(state)
-		if state==true then
-			for _,sub_thread in pairs(_thread.threads) do
-				sub_thread.running.value=sub_thread.resume_state
-			end
-		else
-			for _,sub_thread in pairs(_thread.threads) do
-				sub_thread.resume_state=sub_thread.running.value
-				sub_thread.running.value=false
-			end
-		end
-	end,true)
-	
-	return setmetatable(_thread,eztask.thread)
 end
 
 function eztask:step(dt)
